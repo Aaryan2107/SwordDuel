ec4b3689257ff282c1969f94ca9886bae5f68115
Shubham sapte <shubham24beit@student.mes.ac.in>
2025-10-21 15:07:05 +0530

    i fixed error and implemented player some animation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 from sys import exit
 pygame.init()
-screen = pygame.display.set_mode((1620,1000))
+
+info = pygame.display.Info()
+screen = pygame.display.set_mode((info.current_w, info.current_h), pygame.NOFRAME)
 clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -18,9 +20,20 @@ class Player(pygame.sprite.Sprite):
         idle_9 =pygame.image.load('graphic/Player/Idle/_Idle_r9.png')
         idle_10 =pygame.image.load('graphic/Player/Idle/_Idle_r10.png')
         self.Idle_List = [idle_1,idle_2,idle_3,idle_4,idle_5,idle_6,idle_7,idle_8,idle_9,idle_10]
+        self.scale_image(self.Idle_List)
         self.Idle_index = 0
         self.image = self.Idle_List[self.Idle_index]
-        self.rect = self.image.get_rect()
+
+        # player jump images
+        jump_1 = pygame.image.load('graphic/Player/Jump/jump_1.png').convert_alpha()
+        jump_2 = pygame.image.load('graphic/Player/Jump/jump_2.png').convert_alpha()
+        jump_3 = pygame.image.load('graphic/Player/Jump/jump_3.png').convert_alpha()
+        self.Jump_List = [jump_1,jump_2,jump_3]
+        self.scale_image(self.Jump_List)
+        self.Jump_index = 0
+        self.gravity = 0
+        self.image = self.Jump_List[self.Jump_index]
+       
         # player Normal attack images
         attack_1 = pygame.image.load('graphic/Player/Player_Attack_1/n_attack_1.png').convert_alpha()
         attack_2 = pygame.image.load('graphic/Player/Player_Attack_1/n_attack_2.png').convert_alpha()
@@ -28,22 +41,55 @@ class Player(pygame.sprite.Sprite):
         attack_4 = pygame.image.load('graphic/Player/Player_Attack_1/n_attack_4.png').convert_alpha()
         self.attack_list = [attack_1,attack_2,attack_3,attack_4]
         self.attack_index = 0
-        def player_input(self):
-            key = pygame.key.get_pressed()
-            if key[pygame.K_SPACE]:   
-                self.image = self.attack_list[int(self.attack_index)]
-                self.rect = self.image.get_rect(midbottom = (80,300))
-            else:
-                self.image = self.Idle_List[int(self.Idle_index)]
-                self.rect = self.image.get_rect(midbottom = (80,300))
-    def player_animation_state(self):
+        self.scale_image(self.attack_list)
+        self.images = self.attack_list[self.attack_index]
+        self.rect = self.image.get_rect(midbottom = (120,800))
+    def player_input(self):
+        key = pygame.key.get_pressed()
+        mouse_Button = pygame.mouse.get_pressed()
+        if mouse_Button[0]:   
+            self.image = self.attack_list[int(self.attack_index)]
+            self.rect = self.image.get_rect(midbottom = (120,800))
+        elif key[pygame.K_SPACE] and self.rect.bottom >= 800:
+            self.gravity = -20
+            self.image = self.Jump_List[int(self.Jump_index)]
+            self.rect = self.image.get_rect(midbottom = (120,800))
+        else:
+            self.image = self.Idle_List[int(self.Idle_index)]
+            self.rect = self.image.get_rect(midbottom = (120,800))
     
-        self.attack_index += 0.3
-        if self.attack_index > len(self.attack_list): self.attack_index = 0
-        self.image = self.attack_list[int(self.attack_index)]
+    def scale_image(self,list):
+        for i in range (len(list)):
+            x = int(list[i].get_width() *5)
+            y = int(list[i].get_height() *5)
+            list[i] = pygame.transform.scale(list[i], (x, y))
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 800:	self.rect.bottom = 800
+    def is_idle(self):
+        keys = pygame.key.get_pressed()
+        mouse_Button = pygame.mouse.get_pressed()
+        if mouse_Button[0]:
+            return False
+        return True
+    
+    def player_animation_state(self):
+        if self.is_idle():
+            self.Idle_index += 0.1
+            if self.Idle_index >= len(self.Idle_List): self.Idle_index = 0
+            self.image = self.Idle_List[int(self.Idle_index)]
+        elif pygame.key.get_pressed()[pygame.K_SPACE]:
+            self.Jump_index += 0.1
+            if self.Jump_index >= len(self.Jump_List): self.Jump_index = 0
+            self.image = self.Jump_List[int(self.Jump_index)]
+        else:
+            self.attack_index += 0.1
+            if self.attack_index >= len(self.attack_list): self.attack_index = 0
+            self.image = self.attack_list[int(self.attack_index)]
 
     def update(self):
-        # self.player_input()
+        self.player_input()
         self.player_animation_state()
        
 
@@ -71,13 +117,18 @@ class Background(pygame.sprite.Sprite):
             Level(level)
         
 
-Background_1 = pygame.image.load('graphic/Background/background.png').convert()
-# bg =pygame.transform.scale(Background_1,screen.get_size())
+Background_1 = pygame.image.load('graphic/Background/background.png')
+scale_factor = 0.79
+x = int(Background_1.get_width() * scale_factor)
+y = int(Background_1.get_height() * scale_factor)
+Background_1 = pygame.transform.scale(Background_1, (x, y))
 
 
 
-# player = pygame.sprite.GroupSingle()
-# player.add(Player())
+
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -86,9 +137,11 @@ while True:
         # if event.type == pygame.MOUSEBUTTONDOWN:
         #     if event.key == :
         #         Player.animate()
-    attack_sprite.draw(screen)
-    attack_sprite.update()
-    screen.blit(Background_1,(0,0))
+    
+    screen.blit(Background_1,(0,0))    
+    player.draw(screen)
+    player.update()
+
 
     pygame.display.update()
     clock.tick(60)
