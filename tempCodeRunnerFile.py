db167dfc4c4dc3712de4b6e278e692c4ad691d46
Shubham sapte <shubham24beit@student.mes.ac.in>
2025-10-31 11:19:53 +0530

    testing

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,6 @@
-When player is close enough, boss stops and prepares to attack
-        if
+if player.rect.x > self.rect.x and not self.Direction: # Player is right, Boss is Left
+            self.Direction = True # Change state to Right
+            self.Boss_flip_Animation()
+        elif player.rect.x < self.rect.x and self.Direction: # Player is left, Boss is Right
+            self.Direction = False # Change state to Left
+            self.Boss_flip_Animation()
